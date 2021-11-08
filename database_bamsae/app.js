60b60818ca81d60f1e57e24bdfe70d82616b2847
baethwjd2 <65240657+baethwjd2@users.noreply.github.com>
2021-11-08 20:10:27 +0900

    Update app.js

--- database_bamsae/app.js
+++ database_bamsae/app.js
@@ -1,51 +1,63 @@
 const express = require('express');
 const mysql = require('mysql');
 const app = express();
-const httpPort = 8080;
-const route = require('./router/route.js');
-/* create connection with mysql */
-const connection = mysql.createConnection({  
-    host     : 'localhost',
-    user     : 'root',
-    password : '111111',
-    database : 'products',
-    //multipleStatements: true // for multiple queries
-  });
 
 
-app.get('/',function(req,res){
-    var sql = "SELECT product_id, product_name FROM product";
-    connection.connect();
-    connection.query(sql,function(err,rows){
-        if (err) throw error;
-        res.send(rows);
-        //res.render('list.jade', result);
-      });
-});
+// .env에서 db 서버 credential 가져오기
+require('dotenv').config()
 
-app.get('/id',function(req,res){
-    var sql = "SELECT product_id FROM product";
-    connection.query(sql,function(err,result){
-        if (err) throw err;
-        res.send(result);
-        console.log(res);
-      });
-});
+const db_host = process.env.db_host
+const db_user = process.env.db_user
+const db_password = process.env.db_password
+const db_database = process.env.db_database
+const db_port = process.env.db_port
+const port = process.env.PORT
 
 
-app.put('/id',function(req,res){
-  var id = req.query.id;
-  var name = req.query.name;
-  var sql = `INSERT INTO product VALUES(${id},${name})`;
-  connection.query(sql,function(err,result){
-      if (err) throw err;
-      res.send(result);
-      console.log(res);
-    });
-});
-
-app.use('/',route);
-
-app.listen(httpPort, function(){
-    console.log('running');
+// DB pool 생성
+const db = mysql.createPool({
+	connectionlimit: 100,
+	host: db_host,
+	user: db_user,
+	password: db_password,
+	database: db_database,
+	port: db_port
 })
+
+
+// DB 연결
+db.getConnection((err, connection) => {
+	if (err) throw (err)
+	console.log("DB connected successful")
+})
+
+
+// esj 설정
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs'); 
+
+
+// 에러 핸들러
+app.use(function (req, res, next) {
+	next(createError(404));
+});
+
+app.use(function (err, req, res, next) {
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// 에러 페이지 출력
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+
+// 라우터 연결
+app.get('/', (req, res) => {
+	res.send('Hello World!')
+})
+
+
+// 실행
+app.listen(port, () => console.log(`Server Started on port ${port}`))
+module.exports = app;
